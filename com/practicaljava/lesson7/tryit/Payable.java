436e54a16e9c8585049a316ae6f32fb062f8463b
Natalia Protsko <nprotsko2016@gmail.com>
2017-04-19 00:52:23 +0300

    Lesson 7 Paybale no Ancestor

---
+++ com/practicaljava/lesson7/tryit/Payable.java
@@ -0,0 +1,9 @@
+package com.practicaljava.lesson7.tryit;
+
+/**
+ * Created by nata on 19.04.2017.
+ */
+public interface Payable {
+        int INCREASE_CAP = 20;
+        boolean increasePay(int percent);
+    }
